cdda5218478f26704fec683970264398e8a10df1
themedisons <58108744+themedisons@users.noreply.github.com>
2019-12-07 11:42:20 +0200

    Create 160.py

---
+++ 160.py
@@ -0,0 +1,17 @@
+g = 9.8
+v0 = 0
+t = 0.1
+y = v0 * t - 0.5 * g * t ** 2
+print(y)
+
+t = 1
+y = v0 * t - 0.5 * g * t ** 2
+print(y)
+
+v0 = 5
+t = 0.7
+y = v0 * t - 0.5 * g * t ** 2
+print(y)
+
+v0 = 5; y = 1; y = v0 * t - 0.5 * g * t ** 2
+print('Pec %g sekundem bumba bus %.2f metru augstuma\n' % (t, y))
